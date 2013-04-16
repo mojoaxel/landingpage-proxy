d6e46c1f4bc1d8be9ea4ae5729c55fc0e0ad1dc8
Alexander Wunschik <alex@wunschik.net>
2013-04-16 23:01:45 +0200

    access url working

--- proxy.js
+++ proxy.js
@@ -52,66 +52,95 @@ function deny(response, msg) {
 }
 
 function showLandingPage(response, url) {
+	/*
 	response.writeHead(402); //402 payment required
-   response.write('<html>');
+	response.write('<html>');
 	response.write("<h1>Welcome to Freifunk!</h1>");
 	response.write('<p><a href="192.168.10.40/getaccess?req='+url+'">Weiter zu '+url+'</a></p>');
 	response.end();
+	*/
+	response.writeHead(307, {
+		'Location': 'http://localhost:8080' + 
+					'#?req=' + url + 
+					'&access=http://localhost:9615/getaccess'
+	}); //307 Temporary Redirect
+	response.end();
 }
 
-http.createServer(function(request, response) {
-  var ip = request.connection.remoteAddress;
-  
-  if (!host_allowed(request.url)) {
-    msg = "Host " + request.url + " has been denied by proxy configuration";
-    deny(response, msg);
-    util.puts(msg);
-    return;
-  }
-
-  util.puts(ip + ": " + request.method + " " + request.url);
-  
-  var url = request.url;
-  if (url.indexOf('getaccess') !== -1) {
-  	 ip_list.push(ip);
-  	 var index = url.indexOf('req=');
-  	 var newUrl = url.substr(index, url.length-index);
-  	 request.url = newUrl;
-  }  
-  
-  if (!client_allowed(ip)) {
-    showLandingPage(response, request.url);    
-  } else {
- 
-  var proxy = http.createClient(80, request.headers['host']);
-  var proxy_request = proxy.request(request.method, request.url, request.headers);
-
-  proxy_request.addListener('response', function(proxy_response) {
-    proxy_response.addListener('data', function(chunk) {
-      response.write(chunk, 'binary');
-    });
-    proxy_response.addListener('end', function() {
-      response.end();
-    });
-    response.writeHead(proxy_response.statusCode, proxy_response.headers);
-  });
-  
-  request.addListener('data', function(chunk) {
-    proxy_request.write(chunk, 'binary');
-  });
-  
-  request.addListener('end', function() {
-    proxy_request.end();
-  });
-  
-  request.addListener('error', function(err) {
-    util.puts('error: ' + err);
-    proxy_request.end();
-  });
-  
-  }
-  
-}).listen(8003);
+function startServer() {
+	// wait for clients to ask for access
+	http.createServer(function (req, res) {
+		var ip = req.connection.remoteAddress;
+		console.log("IP added to whitelist: " + ip);
+		ip_list.push(ip);
+			
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.write("Access granted...have fun...");
+		res.end();
+	}).listen(9615);
+	
+	http.createServer(function(request, response) {
+	var ip = request.connection.remoteAddress;
+	
+	/*
+	if (!host_allowed(request.url)) {
+		console.log("ACCESS DENYED!");
+		msg = "Host " + request.url + " has been denied by proxy configuration";
+		deny(response, msg);
+		util.puts(msg);
+		return;
+	}
+	*/
+	
+	util.puts(ip + ": " + request.method + " " + request.url);
+	
+	var url = request.url;
+	/*
+	console.log("URL: ", url);
+	if (url.indexOf('getaccess') !== -1) {
+		console.log("IP added to whitelist: " + ip);
+		ip_list.push(ip);
+		var index = url.indexOf('req=');
+		var newUrl = url.substr(index, url.length-index);
+		request.url = newUrl;
+	}
+	*/
+	
+	if (!client_allowed(ip)) {
+		showLandingPage(response, request.url);    
+	} else {
+	
+	var proxy = http.createClient(80, request.headers['host']);
+	var proxy_request = proxy.request(request.method, request.url, request.headers);
+	
+	  proxy_request.addListener('response', function(proxy_response) {
+	    proxy_response.addListener('data', function(chunk) {
+	      response.write(chunk, 'binary');
+	    });
+	    proxy_response.addListener('end', function() {
+	      response.end();
+	    });
+	    response.writeHead(proxy_response.statusCode, proxy_response.headers);
+	  });
+	  
+	  request.addListener('data', function(chunk) {
+	    proxy_request.write(chunk, 'binary');
+	  });
+	  
+	  request.addListener('end', function() {
+	    proxy_request.end();
+	  });
+	  
+	  request.addListener('error', function(err) {
+	    util.puts('error: ' + err);
+	    proxy_request.end();
+	  });
+	  
+	  }
+	  
+	}).listen(8003);
+}
 
 update_blacklist();
 
+startServer();
